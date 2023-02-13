df8f3987a2a3ef20e6def91b16c3f7661bffd24d
Pepega2005 <75209223+Pepega2005@users.noreply.github.com>
2023-02-13 11:37:49 +0200

    Added menu

--- Lab_Lesson2.cpp
+++ Lab_Lesson2.cpp
@@ -7,9 +7,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
-    cout << "Changes" << endl;
-    cout << "More changes" << endl;
+    char choice;
+
+    cout << "1) Draw a horizontal line\n"
+        << "2) Draw a vertical line\n"
+        << "3) Draw a square\n"
+        << "4) Draw a rectangle\n"
+        << "5) Quit\n";
+
+    cout << "Enter your choice: ";
+    cin >> choice;
 
     return 0;
 }
