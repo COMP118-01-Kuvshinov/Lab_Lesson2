61766dcde7ffa8bee94f1efdab5eba25253471a5
Pepega2005 <75209223+Pepega2005@users.noreply.github.com>
2023-02-20 09:54:26 +0200

    function "drawHorizontalLine" is done

--- Lab_Lesson2.cpp
+++ Lab_Lesson2.cpp
@@ -5,9 +5,12 @@
 
 using namespace std;
 
+void drawHorizontalLine(int, char);
+
 int main()
 {
-    int choice;
+    int choice, length;
+    char ch;
 
     cout << "1) Draw a horizontal line\n"
         << "2) Draw a vertical line\n"
@@ -18,5 +21,38 @@ int main()
     cout << "Enter your choice: ";
     cin >> choice;
 
+    switch (choice)
+    {
+    case 1:
+        cout << "Enter a length of the line: ";
+        cin >> length;
+
+        cout << "Enter a character for the line: ";
+        cin >> ch;
+
+        drawHorizontalLine(length, ch);
+
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    default:
+        break;
+    }
+
     return 0;
+}
+
+void drawHorizontalLine(int length, char ch)
+{
+    for (int i = 0; i < length; i++)
+    {
+        cout << ch;
+    }
+    cout << endl;
 }
