00353a1143d3ac0cf044cd0868e0048e431117ba
Pepega2005 <75209223+Pepega2005@users.noreply.github.com>
2023-04-03 11:43:33 +0300

    Moved functions into new library

---
+++ Shapes.h
@@ -0,0 +1,13 @@
+/**
+* @todo fix comment block
+*/
+
+#ifndef SHAPES_H
+#define SHAPES_H
+
+void drawHorizontalLine(int, char);
+void drawSquare(int, char, int);
+void drawVerticalLine(int, char);
+void drawRectangle(int, int, char, int);
+
+#endif /*SHAPES_H*/
--- driver.cpp
+++ driver.cpp
@@ -9,16 +9,14 @@
 
 #include <iostream>
 #include <cassert>
+// User def. files/libs
+#include "Shapes.h"
 
 using namespace std;
 
 const int MAX_SHAPES = 10;
 
 // functions prototypes
-void drawHorizontalLine(int, char);
-void drawVerticalLine(int, char);
-void drawSquare(int, char, int);
-void drawRectangle(int, int, char, int);
 void drawShapes();
 void initializeArrays(int[MAX_SHAPES], int[MAX_SHAPES], int[MAX_SHAPES], char[MAX_SHAPES], int[MAX_SHAPES]);
 
@@ -157,125 +155,6 @@ int main()
     }
 
     return 0;
-}
-
-/**
-* Function <code>drawHorizontalLine</code> draws a horizontal
-* line.
-* <BR>
-* @param length The length of the horizontal line [>= 2]
-* @param ch The symbol used to draw the horizntal line
-*/
-void drawHorizontalLine(const int length, const char ch)
-{
-    assert(length >= 2);
-    assert(ch >= 33 & ch <= 126);
-
-    int i = 0;
-
-    for (i; i < length; i++)
-    {
-        cout << ch;
-    }
-    cout << endl;
-
-    assert(length == i);
-}
-
-/**
-* Function <code>drawVerticalLine</code> draws a vertical
-* line.
-* <BR>
-* @param height The height of the vertical line [>= 2]
-* @param ch The symbol used to draw the horizntal line
-*/
-void drawVerticalLine(const int height, const char ch)
-{
-    for (int i = 0; i < height; i++)
-    {
-        cout << ch << endl;
-    }
-}
-
-/**
-* Function <code>drawSquare</code> draws a square
-* <BR>
-* @param size The dimention of the square [>= 2]
-* @param ch The symbol used to draw the square
-* @param isFilled The choice for the square to be
-* filled with symbols or not [0 || 1]
-*/
-void drawSquare(const int size, const char ch, const int isFilled)
-{
-    assert(size >= 2); // Pre-condition
-    assert(ch >= 33 && ch <= 126);
-
-    for (int i = 0; i < size; i++)
-    {
-        for (int j = 0; j < size; j++)
-        {
-            if (i == 0 || i == size - 1)
-            {
-                cout << ch << " ";
-            }
-            else
-            {
-                if (j == 0 || j == size - 1)
-                {
-                    cout << ch << " ";
-                }
-                else if (isFilled == 0)
-                {
-                    cout << "  ";
-                }
-                else
-                {
-                    cout << ch << " ";
-                }
-            }
-        }
-        cout << endl;
-    }
-}
-
-/**
-* Function <code>drawRectangle</code> draws a
-* rectangle.
-* <BR>
-* @param height The height of the rectangle [>= 2]
-* @param length The length of the rectangle [>= 2]
-* @param ch The symbol used to draw the rectangle
-* @param isFilled The choice for the rectangle to be
-* filled with symbols or not [0 || 1]
-*/
-void drawRectangle(const int height, const int length, const char ch, const int isFilled)
-{
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < length; j++)
-        {
-            if (i == 0 || i == height - 1)
-            {
-                cout << ch << " ";
-            }
-            else
-            {
-                if (j == 0 || j == length - 1)
-                {
-                    cout << ch << " ";
-                }
-                else if (isFilled == 0)
-                {
-                    cout << "  ";
-                }
-                else
-                {
-                    cout << ch << " ";
-                }
-            }
-        }
-        cout << endl;
-    }
 }
 
 /**
